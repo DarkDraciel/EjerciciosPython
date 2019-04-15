5edcad48a6708d869eea6a01ea9ce5b364db18f3
DarkDraciel <Originofsimetry32@hotmail.com>
2019-04-15 13:55:05 -0500

    GooglePlaces

--- rest.py
+++ rest.py
@@ -1,27 +1,28 @@
 import requests, json 
-  
-def buscar(lat,lon, radio):  
-	api_key = 'AIzaSyAiGXvWHpeV2QXpNtlyREhnwrW7g_kC770'
-	url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=Restaurants"
+ 
+class Rest():
+	def __init__(self, lat,lon, radio):
+		api_key = 'AIzaSyAiGXvWHpeV2QXpNtlyREhnwrW7g_kC770'
+		url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=Restaurants"
+		r = requests.get(url + '&location=' + lat+" "+lon +"&radio"+ radio +'&key=' + api_key)
+		x = r.json()   
+		y = x['results']
+		Csv(y) 
+	 
+class Csv():
+	def __init__(self, y):
+		restaurantes = open('restaurantes.csv','w')
+		for restaurante in y:
+			try:
+				restaurantes.write(str(restaurante['geometry']['location']['lng'])+','+str(restaurante['geometry']['location']['lat'])+" , "+restaurante['name']+'\n')
+			except KeyError as e:
+				restaurantes.write(str(restaurante['geometry']['location']['lng'])+','+str(restaurante['geometry']['location']['lat'])+" , "+restaurante['name']+'\n')
+		restaurantes.close()
 
-#location = input("Location: ")
-#radio = input('Radio: ') 
 
-	r = requests.get(url + '&location=' + lat+" "+lon +"&radio"+ radio +'&key=' + api_key)
-	x = r.json()   
-	y = x['results'] 
-  
-	restaurantes = open('restaurantes.csv','w')
-	for restaurante in y:
-		try:
-			restaurantes.write(str(restaurante['geometry']['location']['lng'])+','+str(restaurante['geometry']['location']['lat'])+" , "+restaurante['name']+'\n')
-		except KeyError as e:
-			restaurantes.write(str(restaurante['geometry']['location']['lng'])+','+str(restaurante['geometry']['location']['lat'])+" , "+restaurante['name']+'\n')
-	restaurantes.close()
+Rest("-33.86094732989272","151.2075474201073","1000")
+		
 
-	for restaurante in range(len(y)): 
-		print(y[i]['name']) 
 
-def llamar():
-	buscar("-33.86094732989272","151.2075474201073","1000")
-	
+		
+		
